3f03e5b9519bfaccb7761b95f654e426c630e1c0
Pratik Singh <141718144+pratik1412001@users.noreply.github.com>
2025-06-23 01:15:46 +0530

    he

--- src/screens/accountScreen/changeDateScreen.js
+++ src/screens/accountScreen/changeDateScreen.js
@@ -13,23 +13,23 @@ const ChangeDateScreen = () => {
             <View style={globle_Style.chang_email}>
                 <Text style={globle_Style.chang_emailtxt}>Your Birhday</Text>
                 <View style={globle_Style.chang_emailitm}>
-                    
+
                     <TextInput
                         placeholder="12/12/2020"
                         placeholderTextColor="#9098B1" // light gray placeholder
                         style={globle_Style.changemail_input}
                     />
-                    <DateNonActive/>
+                    <DateNonActive />
                 </View>
-               
+
             </View>
-           <View style={globle_Style.glo_btnpad}>
-             <TouchableWithoutFeedback>
-                <View style={globle_Style.globle_btn}>
-                    <Text style={globle_Style.gbl_btn}>Save</Text>
-                </View>
-            </TouchableWithoutFeedback>
-           </View>
+            <View style={globle_Style.glo_btnpad}>
+                <TouchableWithoutFeedback>
+                    <View style={globle_Style.globle_btn}>
+                        <Text style={globle_Style.gbl_btn}>Save</Text>
+                    </View>
+                </TouchableWithoutFeedback>
+            </View>
         </View>
     );
 }
